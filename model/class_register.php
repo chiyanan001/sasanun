a9080daeb8b2cb024a474db9b1cf2988a1b46aac
ManopCH <manop.ketpradit@gmail.com>
2018-11-22 22:45:31 +0700

    edit(1) insert database

--- model/class_register.php
+++ model/class_register.php
@@ -14,172 +14,180 @@ class Register {
 	// 	header("Location: $url");
 	// } //TheEnd function
 
-	public function showRegister(){
-		try{
-			$Register = $this->conn->prepare("SELECT register_id,profile.name_lastName,department.departmentName,position.positionName,talent,applicationDate,education.educationName
-FROM register JOIN profile ON register.profile_id = profile.profile_id JOIN department ON register.department_id = department.department_id
-JOIN position ON register.position_id = position.position_id JOIN education ON register.education_id = education.education_id");
-			$Register->execute();
-			$stm = $Register->fetchAll();
-			return $stm;
+		public function showRegister(){
+			try{
+				$Register = $this->conn->prepare("SELECT register_id,profile.name_lastName,department.departmentName,position.positionName,talent,applicationDate,education.educationName
+					FROM register JOIN profile ON register.profile_id = profile.profile_id JOIN department ON register.department_id = department.department_id
+					JOIN position ON register.position_id = position.position_id JOIN education ON register.education_id = education.education_id");
+					$Register->execute();
+					$stm = $Register->fetchAll();
+					return $stm;
+				}
+				catch(PDOException $error){
+					// echo $sql . "<br>" . $error->getMessage();
+					$stm = "Error";
+					return $stm;
+				}
+		}  //TheEnd function
+
+			public function showRegisterById($register_id){
+				try{
+					$Register = $this->conn->prepare("SELECT register_id,profile.name_lastName,department.departmentName,position.positionName,talent,applicationDate,education.educationName
+						FROM register JOIN profile ON register.profile_id = profile.profile_id JOIN department ON register.department_id = department.department_id
+						JOIN position ON register.position_id = position.position_id JOIN education ON register.education_id = education.education_id
+						WHERE register_id = :register_id ");
+						$Register->bindparam(":register_id",$register_id);
+						$Register->execute();
+						$stm = $Register->fetchAll();
+						return $stm;
+					}
+					catch(PDOException $error){
+						// echo $sql . "<br>" . $error->getMessage();
+						$stm = "Error";
+						return $stm;
+					}
+			}  //TheEnd function
+
+			public function addRegister($register_id,$profile_id,$department_id,$position_id,$talent,$applicationDate,$education_id){
+				try{
+					$Register = $this->conn->prepare("INSERT INTO register (register_id,profile_id,department_id,position_id,talent,applicationDate,education_id) VALUES (:register_id,:profile_id,:department_id,:position_id,:talent,:applicationDate,:education_id)");
+					$Register->bindparam(":register_id",$register_id);
+					$Register->bindparam(":profile_id",$profile_id);
+					$Register->bindparam(":department_id",$department_id);
+					$Register->bindparam(":position_id",$position_id);
+					$Register->bindparam(":talent",$talent);
+					$Register->bindparam(":applicationDate",$applicationDate);
+					$Register->bindparam(":education_id",$education_id);
+					$Register->execute();
+				}
+				catch(PDOException $error){
+					echo $sql . "<br>" . $error->getMessage();
+					$stm = "Error";
+					return $stm;
+				}
+			}  //TheEnd function
+
+			public function editRegisterByDepartment($register_id,$department_id){
+				try{
+					$Register = $this->conn->prepare("UPDATE register SET department_id = :department_id WHERE register.register_id = :register_id");
+					$Register->bindparam(":register_id",$register_id);
+					$Register->bindparam(":department_id",$department_id);
+					$Register->execute();
+					// $stm = $editNews->fetchAll();
+					// return $stm;
+				}
+				catch(PDOException $error){
+					// echo $sql . "<br>" . $error->getMessage();
+					$stm = "Error";
+					return $stm;
+				}
+			}  //TheEnd function
+
+			public function editRegisterByPosition($register_id,$position_id){
+				try{
+					$Register = $this->conn->prepare("UPDATE register SET position_id = :position_id WHERE register.register_id = :register_id");
+					$Register->bindparam(":register_id",$register_id);
+					$Register->bindparam(":position_id",$position_id);
+					$Register->execute();
+					// $stm = $editNews->fetchAll();
+					// return $stm;
+				}
+				catch(PDOException $error){
+					// echo $sql . "<br>" . $error->getMessage();
+					$stm = "Error";
+					return $stm;
+				}
+			}  //TheEnd function
+
+			public function editRegisterByTalent($register_id,$talent){
+				try{
+					$Register = $this->conn->prepare("UPDATE register SET talent = :talent WHERE register.register_id = :register_id");
+					$Register->bindparam(":register_id",$register_id);
+					$Register->bindparam(":talent",$talent);
+					$Register->execute();
+					// $stm = $editNews->fetchAll();
+					// return $stm;
+				}
+				catch(PDOException $error){
+					// echo $sql . "<br>" . $error->getMessage();
+					$stm = "Error";
+					return $stm;
+				}
+			}  //TheEnd function
+
+			public function editRegisterByEducation($register_id,$education_id){
+				try{
+					$Register = $this->conn->prepare("UPDATE register SET education_id = :education_id WHERE register.register_id = :register_id");
+					$Register->bindparam(":register_id",$register_id);
+					$Register->bindparam(":education_id",$education_id);
+					$Register->execute();
+					// $stm = $editNews->fetchAll();
+					// return $stm;
+				}
+				catch(PDOException $error){
+					// echo $sql . "<br>" . $error->getMessage();
+					$stm = "Error";
+					return $stm;
+				}
+			}  //TheEnd function
+
+			public function deleteRegister($register_id){
+				try{
+					$Register = $this->conn->prepare("DELETE FROM register WHERE register_id = :register_id");
+					$Register->bindparam(":register_id",$register_id);
+					$Register->execute();
+					// $stm = $deleteNews->fetchAll();
+					// return $stm;
+				}
+				catch(PDOException $error){
+					// echo $sql . "<br>" . $error->getMessage();
+					$stm = "Error";
+					return $stm;
+				}
+			}  //TheEnd function
+
+
+
 		}
-		catch(PDOException $error){
-			// echo $sql . "<br>" . $error->getMessage();
-			$stm = "Error";
-			return $stm;
+
+
+
+
+
+
+		// Test function();
+
+		$n = new Register();
+
+		// $n->showRegister();
+		// print_r($n->showRegister());
+
+		// $n->showRegisterById("081025610001");
+		// print_r($n->showRegisterById("081025610001"));
+
+		// $n->addRegister('081025610003', '1719900409932', '12', '101', '123', '2018-11-01', '102');
+
+		// $n->editRegisterByDepartment("081025610001",12);
+
+		// $n->editRegisterByPosition("081025610001",201);
+
+		// $n->editRegisterByTalent("081025610001",12);
+		//
+		// $n->editRegisterByEducation("081025610001",102);
+
+		// $n->deleteRegister("081025610001");
+
+		if($_POST){
+				echo "id : ".$_POST['id']."id_emp : ".$_POST['id_emp']."dep : ".$_POST['dep']."pos : ".$_POST['pos']."exp : ".$_POST['exp']."datepicker : ".$_POST['datepicker']."inputState : ".$_POST['inputState'];
+				echo $n->addRegister($_POST['id'], $_POST['id_emp'], $_POST['dep'], $_POST['pos'], $_POST['exp'], $_POST['datepicker'], $_POST['inputState']);
+	  }else{
+			echo "never data!!";
 		}
-	}  //TheEnd function
-
-	public function showRegisterById($register_id){
-		try{
-			$Register = $this->conn->prepare("SELECT register_id,profile.name_lastName,department.departmentName,position.positionName,talent,applicationDate,education.educationName
-FROM register JOIN profile ON register.profile_id = profile.profile_id JOIN department ON register.department_id = department.department_id
-JOIN position ON register.position_id = position.position_id JOIN education ON register.education_id = education.education_id
-WHERE register_id = :register_id ");
-			$Register->bindparam(":register_id",$register_id);
-			$Register->execute();
-			$stm = $Register->fetchAll();
-			return $stm;
-		}
-		catch(PDOException $error){
-			// echo $sql . "<br>" . $error->getMessage();
-			$stm = "Error";
-			return $stm;
-		}
-	}  //TheEnd function
-
-	public function addRegister($register_id,$profile_id,$department_id,$position_id,$talent,$applicationDate,$education_id){
-		try{
-			$Register = $this->conn->prepare("INSERT INTO register (register_id,profile_id,department_id,position_id,talent,applicationDate,education_id) VALUES (:register_id,:profile_id,:department_id,:position_id,:talent,:applicationDate,:education_id)");
-			$Register->bindparam(":register_id",$register_id);
-			$Register->bindparam(":profile_id",$profile_id);
-			$Register->bindparam(":department_id",$department_id);
-      $Register->bindparam(":position_id",$position_id);
-      $Register->bindparam(":talent",$talent);
-      $Register->bindparam(":applicationDate",$applicationDate);
-      $Register->bindparam(":education_id",$education_id);
-			$Register->execute();
-		}
-		catch(PDOException $error){
-			// echo $sql . "<br>" . $error->getMessage();
-			$stm = "Error";
-			return $stm;
-		}
-	}  //TheEnd function
-
-	public function editRegisterByDepartment($register_id,$department_id){
-		try{
-			$Register = $this->conn->prepare("UPDATE register SET department_id = :department_id WHERE register.register_id = :register_id");
-			$Register->bindparam(":register_id",$register_id);
-			$Register->bindparam(":department_id",$department_id);
-			$Register->execute();
-			// $stm = $editNews->fetchAll();
-			// return $stm;
-		}
-		catch(PDOException $error){
-			// echo $sql . "<br>" . $error->getMessage();
-			$stm = "Error";
-			return $stm;
-		}
-	}  //TheEnd function
-
-	public function editRegisterByPosition($register_id,$position_id){
-		try{
-			$Register = $this->conn->prepare("UPDATE register SET position_id = :position_id WHERE register.register_id = :register_id");
-			$Register->bindparam(":register_id",$register_id);
-			$Register->bindparam(":position_id",$position_id);
-			$Register->execute();
-			// $stm = $editNews->fetchAll();
-			// return $stm;
-		}
-		catch(PDOException $error){
-			// echo $sql . "<br>" . $error->getMessage();
-			$stm = "Error";
-			return $stm;
-		}
-	}  //TheEnd function
-
-	public function editRegisterByTalent($register_id,$talent){
-		try{
-			$Register = $this->conn->prepare("UPDATE register SET talent = :talent WHERE register.register_id = :register_id");
-			$Register->bindparam(":register_id",$register_id);
-			$Register->bindparam(":talent",$talent);
-			$Register->execute();
-			// $stm = $editNews->fetchAll();
-			// return $stm;
-		}
-		catch(PDOException $error){
-			// echo $sql . "<br>" . $error->getMessage();
-			$stm = "Error";
-			return $stm;
-		}
-	}  //TheEnd function
-
-	public function editRegisterByEducation($register_id,$education_id){
-		try{
-			$Register = $this->conn->prepare("UPDATE register SET education_id = :education_id WHERE register.register_id = :register_id");
-			$Register->bindparam(":register_id",$register_id);
-			$Register->bindparam(":education_id",$education_id);
-			$Register->execute();
-			// $stm = $editNews->fetchAll();
-			// return $stm;
-		}
-		catch(PDOException $error){
-			// echo $sql . "<br>" . $error->getMessage();
-			$stm = "Error";
-			return $stm;
-		}
-	}  //TheEnd function
-
-	public function deleteRegister($register_id){
-		try{
-			$Register = $this->conn->prepare("DELETE FROM register WHERE register_id = :register_id");
-			$Register->bindparam(":register_id",$register_id);
-			$Register->execute();
-			// $stm = $deleteNews->fetchAll();
-			// return $stm;
-		}
-		catch(PDOException $error){
-			// echo $sql . "<br>" . $error->getMessage();
-			$stm = "Error";
-			return $stm;
-		}
-	}  //TheEnd function
-
-
-
-}
 
 
 
 
 
 
-// Test function();
 
-$n = new Register();
-
-// $n->showRegister();
-// print_r($n->showRegister());
-
-// $n->showRegisterById("081025610001");
-// print_r($n->showRegisterById("081025610001"));
-
-// $n->addRegister('081025610003', '1719900409932', '12', '101', '123', '2018-11-01', '102');
-
-// $n->editRegisterByDepartment("081025610001",12);
-
-// $n->editRegisterByPosition("081025610001",201);
-
-// $n->editRegisterByTalent("081025610001",12);
-//
-// $n->editRegisterByEducation("081025610001",102);
-
-// $n->deleteRegister("081025610001");
-
-
-
-
-
-
-?>
+		?>
--- view/register.php
+++ view/register.php
@@ -7,10 +7,10 @@ include("head.php");
 }
 </style>
 </style>
- <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/gijgo@1.9.10/js/gijgo.min.js" type="text/javascript"></script>
-    <link href="https://cdn.jsdelivr.net/npm/gijgo@1.9.10/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/gijgo@1.9.10/js/gijgo.min.js" type="text/javascript"></script>
+<link href="https://cdn.jsdelivr.net/npm/gijgo@1.9.10/css/gijgo.min.css" rel="stylesheet" type="text/css" />
 <!------------------------------------------------------Header------------------------------------------------------>
 
 <div class="content-wrapper"> <!-- start1 -->
@@ -20,77 +20,108 @@ include("head.php");
       <h4> สมัครงาน </h4>
     </ol>
 
-   <hr>
-  
-   <div class="card mb-3">
-    <div class="card-header">รับสมัครงาน</div>
-    <div class="card-body">
-    <form class="was-validated">
-    <form>
+    <hr>
 
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">เลขที่ใบสมัคร</label>
-      <input type="" class="form-control" id="" placeholder="">
-    </div>
-    <div class="form-group col-md-6">
-      <label for="inputPassword4">เลขบัตรประชาชน</label>
-      <input type="" class="form-control" id="" placeholder="">
-    </div>
-  </div>
+    <div class="card mb-3">
+      <div class="card-header">รับสมัครงาน</div>
+      <div class="card-body">
+        <form class="was-validated" action="../model/class_register.php" method="post">
 
- <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">รหัสแผนก</label>
-      <input type="" class="form-control" id="" placeholder="">
-    </div>
-    <div class="form-group col-md-6">
-      <label for="inputPassword4">รหัสตำแหน่ง</label>
-      <input type="" class="form-control" id="" placeholder="">
-    </div>
-  </div>
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="inputEmail4">เลขที่ใบสมัคร</label>
+                <input type="text" class="form-control" id="id" name="id"placeholder="" value="">
+              </div>
+              <div class="form-group col-md-6">
+                <label for="inputPassword4">เลขบัตรประชาชน</label>
+                <input type="text" class="form-control" id="id_emp" name="id_emp" placeholder="">
+              </div>
+            </div>
 
-    <div class="form-group">
-    <label for="exampleFormControlTextarea1">ความสามารถ</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-  </div>
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="inputEmail4">รหัสแผนก</label>
+                <input type="text" class="form-control" id="dep" name="dep" placeholder="">
+              </div>
+              <div class="form-group col-md-6">
+                <label for="inputPassword4">รหัสตำแหน่ง</label>
+                <input type="text" class="form-control" id="pos" name="pos" placeholder="">
+              </div>
+            </div>
 
- <div class="form-group col-md-12">
-                  <div class="form-row">
-                    <div class="form-group col-md-6">
-                      <label>วันที่สมัคร</label>
-   <input id="datepicker" width="276" />
-    <script>
-        $('#datepicker').datepicker({
-            uiLibrary: 'bootstrap4'
-        });
-    </script>
-                    </div>
+            <div class="form-group">
+              <label for="exampleFormControlTextarea1">ความสามารถ</label>
+              <textarea class="form-control" id="exp" name="exp" rows="3"></textarea>
+            </div>
 
-    <div class="form-group col-md-4">
-      <label for="inputState">การศึกษา</label>
-      <select id="inputState" class="form-control">
-        <option selected></option>
-        <option>ชั้นมัธยมศึกษาชั้นปีที่3</option>
-        <option>ชั้นมัธยมศึกษาชั้นปีที่6</option>
-        <option>ปริญญาตรี</option> 
-        <option>ปริญญาโท</option>
-        <option>ปริญญาเอก</option>
-      </select>
-    </div>
-    <div class="form-group col-md-4">
-  <button type="submit" class="btn btn-primary">ส่งใบสมัคร</button>
-</form>
 
-                
-                
-                
-    
+            <div class=" col-md-12">
+
+              <div class="form-row">
+                <div class=" col-md-6">
+                  <label>วันที่สมัคร</label>
+                  <input id="datepicker" name="datepicker" width="276" value="--/--/----">
+                  <script>
+                  $('#datepicker').datepicker({
+                    uiLibrary: 'bootstrap4'
+                  });
+                  </script>
+                </div>
+                <div class="col-md-4">
+                  <label for="inputState">การศึกษา</label>
+                  <select id="inputState" name="inputState" class="form-control">
+                    <option selected></option>
+                    <option>ชั้นมัธยมศึกษาชั้นปีที่3</option>
+                    <option>ชั้นมัธยมศึกษาชั้นปีที่6</option>
+                    <option>ปริญญาตรี</option>
+                    <option>ปริญญาโท</option>
+                    <option>ปริญญาเอก</option>
+                  </select>
+              </div>
+
+            </div>
+          </div>
+          <div class="col-md-2">
+            <br>
+            <button type="submit" class="btn btn-primary" name="submit" value="submit">ส่งใบสมัคร</button>
+          </div>
+        </form>
+        <!--------------------TheEnd form-------------------->
+        </div>
       </div>
     </div>
-  </div> <!--------------------TheEnd Row-------------------->
+  </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="../vendor/jquery/jquery.min.js"></script>
+  <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- Core plugin JavaScript-->
+  <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
+  <!-- Page level plugin JavaScript-->
+  <script src="../vendor/chart.js/Chart.min.js"></script>
+  <script src="../vendor/datatables/jquery.dataTables.js"></script>
+  <script src="../vendor/datatables/dataTables.bootstrap4.js"></script>
+  <!-- Custom scripts for all pages-->
+  <script src="../js/sb-admin.min.js"></script>
+  <!-- Custom scripts for this page-->
+  <script src="../js/sb-admin-datatables.min.js"></script>
+  <script src="../js/sb-admin-charts.min.js"></script>
+
+  <script type="text/javascript">
+  $( document ).ready(function() {
+    // setInterval(function () {
+    //   console.log($('#datepicker').val());
+    // }, 1000);
+
+  });
+
+  </script>
 
 
+
+
+</body>
+</html>
 
 
 
@@ -121,5 +152,5 @@ include("head.php");
 
 
 <?php
-include("footer.php");
+// include("footer.php");
 ?>
